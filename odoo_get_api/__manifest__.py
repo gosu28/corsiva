d13bb0473386a579a67cfe8b5b02ae35f805366e
minhminh191000 <minh2k3k4k@gmail.com>
2024-06-13 00:04:37 +0700

    [add]

--- odoo_get_api/__manifest__.py
+++ odoo_get_api/__manifest__.py
@@ -7,6 +7,7 @@
     'category': 'Uncategorized',
     'version': '16.0.0.0',
     'depends': [
+        'sale'
     ],
     'data': [
         'security/ir.model.access.csv',
--- odoo_get_api/controllers/controllers.py
+++ odoo_get_api/controllers/controllers.py
@@ -1,6 +1,7 @@
 from odoo import http
 from odoo.http import request
 import json
+from odoo.exceptions import AccessError
 
 
 class OdooApi(http.Controller):
@@ -23,7 +24,7 @@ class OdooApi(http.Controller):
         return result
 
     @http.route('/logout-app', type='json', auth='public', csrf=False, methods=['POST'])
-    def login_app(self, **kw):
+    def logout_app(self, **kw):
         result = request.httprequest.data
         data = json.loads(result)
         login = data.get('login')
@@ -46,27 +47,70 @@ class OdooApi(http.Controller):
             }
         return result
 
-    @http.route('/get-order-app', type='json', auth='public', csrf=False, methods=['POST'])
-    def login_app(self, **kw):
-        result = request.httprequest.data
-        data = json.loads(result)
-        login = data.get('login')
-        password = data.get('password')
+    @http.route('/get-list-order', type='json', auth='public', csrf=False, methods=['GET'])
+    def list_order(self, **kw):
+        try:
+            order = request.env['sale.order'].sudo().search([])
+            data = []
+            for rec in order:
+                order_dict = {
+                    'name': rec.name,
+                    'customer': rec.partner_id.name,
+                    'phone': rec.partner_id.phone,
+                    'total': rec.amount_total,
+                    'state': rec.state
+                }
+                data.append(order_dict)
 
-        login = request.env['login.app'].sudo().search([('login', '=', login),
-                                                        ('password', '=', password)], limit=1)
-        if login:
-            login.logout_api()
             result = {
-                'data': {},
+                'data': data,
                 'status': 200,
-                'msg': 'Logout Successful',
+                'msg': 'Load data Successful',
             }
-        else:
+        except AccessError as e:
             result = {
-                'data': {},
+                'data': [],
                 'status': 400,
-                'msg': 'Logout Fail',
+                'msg': 'Load data Fail',
+            }
+        return result
+
+    @http.route('/get-detail-order', type='json', auth='public', csrf=False, methods=['POST'])
+    def get_detail_order(self, **kw):
+        try:
+            result = request.httprequest.data
+            data = json.loads(result)
+            so_name = data.get('name')
+            order = request.env['sale.order'].sudo().search([('name', '=', so_name)], limit=1)
+            order_dict = {
+                'name': order.name,
+                'customer': order.partner_id.name,
+                'phone': order.partner_id.phone,
+                'total': order.amount_total,
+                'state': order.state,
+                'salesperson': order.user_id.name,
+                'sale_phone': order.user_id.phone,
+                'order_line': []
+            }
+            for line in order.order_line:
+                lines = {
+                    'name': line.product_id.name,
+                    'quantity': line.product_uom_qty,
+                    'price_unit': line.price_unit,
+                    'price_subtotal': line.price_subtotal,
+                }
+                order_dict['order_line'].append(lines)
+
+            result = {
+                'data': order_dict,
+                'status': 200,
+                'msg': 'Load data Successful',
+            }
+        except AccessError as e:
+            result = {
+                'data': [],
+                'status': 400,
+                'msg': 'Load data Fail',
             }
         return result
 
